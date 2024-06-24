e1890ab59018ee149a642663284d91780b1b004f
Jhonatan-Andrade <jhonatanjaq@gmai.com>
2024-06-24 16:27:05 -0300

    adicionar stars

--- js/script.js
+++ js/script.js
@@ -97,15 +97,57 @@ function injectListRecipesInDom(params) {
         img.alt = params.title,
         img.src = params.img
 
+        const divData = document.createElement("div")
+        divData.className = "data"
+
         const h1 = document.createElement("h1")
-         h1.className = "itenRecipesTitle"
-  
+        h1.className = "itenRecipesTitle"
+
+        const divStars = document.createElement("div")
+        divStars.className = "boxStars"
+
+        const p = document.createElement("p")
+        p.className = "starts"
         
+
         ul.appendChild(li)
         li.appendChild(a)
         a.appendChild(img)
-        a.appendChild(h1)
+        a.appendChild(divData)
+        divData.appendChild(h1)
         h1.innerText= params.title
+
+        divData.appendChild(divStars)
+
+        const arrayIcons = ['<i class=" stars fa-solid fa-star"></i><i class="fa-regular stars fa-star"></i><i class="fa-regular stars fa-star"></i><i class="fa-regular stars fa-star"></i><i class="fa-regular stars fa-star"></i><i class="fa-regular stars fa-star"></i>','<i class=" stars fa-solid fa-star"></i><i class=" stars fa-solid fa-star"></i><i class="fa-regular stars fa-star"></i><i class="fa-regular stars fa-star"></i><i class="fa-regular stars fa-star"></i>','<i class=" stars fa-solid fa-star"></i><i class=" stars fa-solid fa-star"></i><i class=" stars fa-solid fa-star"></i><i class="fa-regular stars fa-star"></i><i class="fa-regular stars fa-star"></i>','<i class=" stars fa-solid fa-star"></i><i class=" stars fa-solid fa-star"></i><i class=" stars fa-solid fa-star"></i><i class=" stars fa-solid fa-star"></i><i class="fa-regular stars fa-star"></i>','<i class=" stars fa-solid fa-star"></i><i class=" stars fa-solid fa-star"></i><i class=" stars fa-solid fa-star"></i><i class=" stars fa-solid fa-star"></i><i class=" stars fa-solid fa-star"></i>']
+
+        const n =  parseInt(params.stars)
+
+
+        switch(n) {
+            case 1: {
+                divStars.innerHTML = arrayIcons[0]
+                break;
+            }
+            case 2: {
+                divStars.innerHTML = arrayIcons[1]
+                break;
+            }
+            case 3: {
+                divStars.innerHTML =arrayIcons[2]
+                break;
+            }
+            case 4: {
+                divStars.innerHTML =arrayIcons[3]
+                break;
+            }
+            case 5: {
+                divStars.innerHTML =arrayIcons[4]
+                break;
+            }
+        }
+
+        
 }
 
 // --filter-------------------------------------------------------------------------------------------------------------------
